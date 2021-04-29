0caeb4e4b38b59b48356e79ac66f4ccfb8c1496a
Tyler McFarland <mcfarlandtyler@LIVE.COM>
2021-04-29 18:23:13 -0500

    WIP: Set up necessary Packages, gitignores, and classes

---
+++ TechnicalShowcase/TechnicalShowcase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PhotoAlbum.ValidationHandler;
+
+namespace PhotoAlbum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string albumId = string.Empty;
+
+            Console.Write("Please enter an album ID: ");
+            albumId = Console.ReadLine();
+
+        }
+    }
+}
---
+++ TechnicalShowcase/TechnicalShowcase/ValidationHandler/ValidationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.ValidationHandler
+{
+    class ValidationHandler
+    {
+
+        public static bool CallAllValidators(string albumId)
+        {
+            bool passedValidation = false;
+
+
+            return passedValidation;
+        }
+    }
+}
